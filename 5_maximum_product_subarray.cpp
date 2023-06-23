9aec9f1bb5f0a0bfa3a09046c93cbd6831911058
honestfreak <me21b006@iittp.ac.in>
2023-06-23 12:04:14 +0530

    leetcode 152

--- 5_maximum_product_subarray.cpp
+++ 5_maximum_product_subarray.cpp
@@ -12,7 +12,7 @@ int maxProduct(vector<int>& v) {
    int ans = v[0];
 
     for(int i = 1 ; i<v.size() ; i++){
-       if(v[i] < 0) { swap(maxp, minp); }
+       if(v[i] < 0) swap(maxp, minp);  // max and min swaps if negative value is encountered
        maxp = max(v[i], maxp*v[i]);
        minp = min(v[i] , minp*v[i]);
        ans = max(ans, maxp);
